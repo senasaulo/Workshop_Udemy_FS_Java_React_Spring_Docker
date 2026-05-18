2798a4ab767cc60f487b7aa105dc0f8a694ac660
senasaulo <srs3@aluno.ifal.edu.br>
2026-05-18 14:54:54 -0300

    Mapeamento JPA para a entitade Image

---
+++ src/main/java/github/senasaulo/imageliteapi/domain/entity/Image.java
@@ -0,0 +1,50 @@
+package github.senasaulo.imageliteapi.domain.entity;
+
+import java.time.LocalDate;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import github.senasaulo.imageliteapi.domain.enums.ImageExtension;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column
+    private String name;
+    @Column
+    private long size;
+    @Column
+    @Enumerated(EnumType.STRING)
+    private ImageExtension extension;
+    @CreatedDate
+    private LocalDate uploadDate;
+    @Column
+    private String tags;
+    @Column
+    @Lob
+    private byte[] file;
+}
---
+++ src/main/java/github/senasaulo/imageliteapi/domain/enums/ImageExtension.java
@@ -0,0 +1,12 @@
+package github.senasaulo.imageliteapi.domain.enums;
+
+public enum ImageExtension {
+
+    JPG,
+    PNG, 
+    GIF, 
+    BMP, 
+    TIFF, 
+    WEBP,
+    JPEG
+}
