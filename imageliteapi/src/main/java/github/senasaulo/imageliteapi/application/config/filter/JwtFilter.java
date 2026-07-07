89770f92b1670726c5af0626993978eac4ef4971
senasaulo <srs3@aluno.ifal.edu.br>
2026-07-07 15:24:52 -0300

    Configuração de filtro para urls abertas

--- imageliteapi/src/main/java/github/senasaulo/imageliteapi/application/config/filter/JwtFilter.java
+++ imageliteapi/src/main/java/github/senasaulo/imageliteapi/application/config/filter/JwtFilter.java
@@ -71,4 +71,9 @@ public class JwtFilter extends OncePerRequestFilter {
         return null;
     }
 
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return request.getRequestURI().contains("/v1/users");
+    }
+
 }
